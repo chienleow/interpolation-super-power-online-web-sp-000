cf02f9358ff3acad9356943ef782c4250056ba52
C Leow <chienleow1@gmail.com>
2019-05-13 02:47:52 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
 def display_rainbow(color)
 puts "R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, G: #{color[3]}, B: #{color[4]}, I: #{color[5]}, V:#{color[6]}"
 end
-
-display_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
-display_rainbow(color)
-=======
-def
-display_rainbow(color)
-display_rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-puts "The first color in the alphabet is: #{color[0]}"
-puts "The second letter in the alphabet is: #{color[1]}"
-puts "The third letter in the alphabet is: #{lcolor[2]}"
-end
->>>>>>> 56c751d2b161fe246ce17392b60eafa99d4835bf
